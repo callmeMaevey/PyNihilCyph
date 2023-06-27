35b87a4e069bc4a415bcebe754aa59456587e8c7
Maevey <70716966+callmeMaevey@users.noreply.github.com>
2023-06-27 00:39:11 -0400

    ⚙️ made sytax more pythonic

--- main.py
+++ main.py
@@ -5,8 +5,8 @@
 from typing import List
 
 
-def getuserinfo() -> dict[str, str]:
-    userinfo = {
+def get_user_info() -> dict[str, str]:
+    userinfo: dict[str, str] = {
         "PlainText": "",
         "Key": "",
         "Path to Polybius Square": ""
@@ -17,10 +17,10 @@ def getuserinfo() -> dict[str, str]:
     return userinfo
 
 
-def readsquare(squarepath) -> list[str]:
+def read_square(square_path) -> list[str]:
     square: list[str] = []
-    print(f'reading: {squarepath}')
-    with open(squarepath, encoding="utf-8") as file:
+    print(f'reading: {square_path}')
+    with open(square_path, encoding="utf-8") as file:
         for line in file:
             tmp: str = line.upper().replace("\n", "").replace(' ', '')
             # cleanup in case of sloppy users
@@ -29,7 +29,7 @@ def readsquare(squarepath) -> list[str]:
     return square
 
 
-def getnum(square: list[str], char: str) -> int:
+def get_num(square: list[str], char: str) -> int:
     i: int = -1
     j: int = -1
     for line in square:
@@ -41,15 +41,15 @@ def getnum(square: list[str], char: str) -> int:
         return int(str(i + 1) + str(j + 1))
 
 
-def getallnums(square: list[str], plaintext: str) -> list[int]:
+def get_all_nums(square: list[str], plaintext: str) -> list[int]:
     out: list[int] = []
     for char in plaintext:
-        num: int = getnum(square, char)
+        num: int = get_num(square, char)
         out.append(num)
     return out
 
 
-def customVecAdder(pt: list[int], key: list[int]) -> list[int]:
+def custom_vec_adder(pt: list[int], key: list[int]) -> list[int]:
     out: list[int] = []
     while len(key) < len(pt):
         key += key
@@ -59,11 +59,18 @@ def customVecAdder(pt: list[int], key: list[int]) -> list[int]:
 
 
 if __name__ == '__main__':
-    info: dict[str, str] = getuserinfo()
-    polybius: list[str] = readsquare(info["Path to Polybius Square"])
-    ptnums: list[int] = getallnums(polybius, info["PlainText"])
-    keynums: list[int] = getallnums(polybius, info["Key"])
-    cryptictext: list[int] = customVecAdder(ptnums, keynums)
+    info: dict[str, str] = get_user_info()
+    try:
+        polybius: list[str] = read_square(info["Path to Polybius Square"])
+    except FileNotFoundError:
+        print("File not found, Exiting...")
+        exit(-1)
 
-    print(cryptictext)
+    print(
+        custom_vec_adder(
+            get_all_nums(polybius, info["PlainText"]),
+            get_all_nums(polybius, info["Key"])
+        )
+    )
+
     input("Press Enter to Close...")
